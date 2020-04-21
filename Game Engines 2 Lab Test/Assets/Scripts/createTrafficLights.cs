574e656bfcde039405ca3e4544d9007e27337b0a
Emma Neeson <emmaneesoneire@gmail.com>
2020-04-21 14:47:36 +0100

    Change red green yellow
    traffic light ai

---
+++ Game Engines 2 Lab Test/Assets/Scripts/createTrafficLights.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createTrafficLights : MonoBehaviour
+{
+    List<GameObject> Cylinders = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cylindersCreatedInACircle();
+    }
+
+    public float radius = 10;
+
+    void cylindersCreatedInACircle()
+    {
+        float theta = (Mathf.PI * 2.0f) / 10f;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 pos = new Vector3(Mathf.Sin(theta * i) * radius, 0, Mathf.Cos(theta * i) * radius);
+            pos = transform.TransformPoint(pos);
+
+            GameObject trafficL = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+
+            trafficL.transform.position = pos;
+            trafficL.transform.parent = this.transform;
+
+            Cylinders.Add(trafficL);
+        }
+
+        foreach (GameObject go in Cylinders)
+        {
+            go.AddComponent<trafficLightAI>();
+            go.AddComponent<Renderer>();
+        }
+    }
+}
---
+++ Game Engines 2 Lab Test/Assets/Scripts/trafficLightAI.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trafficLightAI : MonoBehaviour
+{
+    public bool isGreen = false;
+
+    // Update is called once per frame
+    void Start()
+    {
+        float startingLight = Random.Range(1, 9);
+
+        if( startingLight < 4)
+        {
+            StartCoroutine(greenLight());
+        }
+        else if (startingLight >=4 && startingLight < 7)
+        {
+            StartCoroutine(yellowLight());
+        }
+        else if (startingLight >= 7)
+        {
+            StartCoroutine(redLight());
+        }
+    }
+
+    public IEnumerator greenLight()
+    {
+
+        GetComponent<Renderer>().material.color = Color.green;
+        isGreen = true;
+
+        yield return new WaitForSeconds(Random.Range(5f, 10f));
+
+        StartCoroutine(yellowLight());
+    }
+
+    public IEnumerator yellowLight()
+    {
+        GetComponent<Renderer>().material.color = Color.yellow;
+        isGreen = false;
+
+        yield return new WaitForSeconds(4f);
+
+        StartCoroutine(redLight());
+    }
+
+    public IEnumerator redLight()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+        isGreen = false;
+
+        yield return new WaitForSeconds(Random.Range(5f, 10f));
+
+        StartCoroutine(greenLight());
+    }
+}
